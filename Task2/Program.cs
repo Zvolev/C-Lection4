c294213f77cbe0a1c4f1741aa6367fcdfaeec168
Evgenii <zvolev@mail.ru>
2022-10-18 00:39:04 +0300

    Добавил задачи из 4ой лекции

---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+void MatrixPrinr(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+void MatrixFill( int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 20);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+MatrixPrinr(matrix);
+MatrixFill(matrix);
+System.Console.WriteLine();
+MatrixPrinr(matrix);
+
---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+double Factorial(int n)
+{
+    if (n==1)
+    {
+        return 1;
+    }
+    else return n * Factorial(n-1);
+}
+
+for (int i = 1; i < 31; i++)
+{
+    System.Console.Write($"{i}  ");
+    System.Console.WriteLine(Factorial(i));
+}
+
+//System.Console.WriteLine($"{n}  {Factorial(n-1)*n}");
